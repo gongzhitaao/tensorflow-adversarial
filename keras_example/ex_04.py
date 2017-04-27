82cb43099a387f7be00a53b9aea4a83a61354c01
Zhitao Gong <zhitaao.gong@gmail.com>
2017-04-27 16:22:15 -0500

    keras jsma pair=True example

--- keras_example/ex_04.py
+++ keras_example/ex_04.py
@@ -52,7 +52,7 @@ sess = tf.InteractiveSession()
 K.set_session(sess)
 
 
-if True:
+if False:
     print('\nLoading model')
     model = load_model('model/ex_04.h5')
 else:
@@ -86,7 +86,7 @@ x = tf.placeholder(tf.float32, shape=(None, img_rows, img_cols,
                                       img_chas))
 y = tf.placeholder(tf.float32, shape=(None, nb_classes))
 target = tf.placeholder(tf.int32, ())
-x_adv = jsma(model, x, target, nb_epoch=0.05, pair=True, min_proba=0.8)
+x_adv = jsma(model, x, target, epochs=0.05, pair=True, min_proba=0.8)
 
 
 print('\nTest against clean data')
