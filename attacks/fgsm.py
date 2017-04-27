fcb3ea7c46f8ac98f6d0f0563aa27dfce38ebe88
Zhitao Gong <zhitaao.gong@gmail.com>
2017-04-27 14:54:50 -0500

    update api, remove dependence on keras

--- attacks/fgsm.py
+++ attacks/fgsm.py
@@ -1,24 +1,24 @@
 import tensorflow as tf
 
 
-def fgsm(model, x, eps=0.01, nb_epoch=1, clip_min=0., clip_max=1.):
+def fgsm(model, x, eps=0.01, epochs=1, clip_min=0., clip_max=1.):
 
     x_adv = tf.identity(x)
+
     ybar = model(x_adv)
     yshape = tf.shape(ybar)
     ydim = yshape[1]
-
     indices = tf.argmax(ybar, axis=1)
-    eps = tf.abs(eps)
     target = tf.one_hot(indices, ydim, on_value=clip_max,
                         off_value=clip_min)
 
+    eps = tf.abs(eps)
+
     def _cond(x_adv, i):
-        return tf.less(i, nb_epoch)
+        return tf.less(i, epochs)
 
     def _body(x_adv, i):
-        ybar = model(x_adv)
-        logits, = ybar.op.inputs
+        ybar, logits = model(x_adv, logits=True)
         loss = tf.nn.softmax_cross_entropy_with_logits(
             labels=target, logits=logits)
         dy_dx, = tf.gradients(loss, x_adv)
@@ -26,7 +26,6 @@ def fgsm(model, x, eps=0.01, nb_epoch=1, clip_min=0., clip_max=1.):
         x_adv = tf.clip_by_value(x_adv, clip_min, clip_max)
         return x_adv, i+1
 
-    i = tf.Variable(0)
-    x_adv, i = tf.while_loop(_cond, _body, (x_adv, i),
+    x_adv, i = tf.while_loop(_cond, _body, (x_adv, 0),
                              back_prop=False, name='fgsm')
     return x_adv
