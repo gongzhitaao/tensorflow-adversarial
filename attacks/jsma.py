4636009823446e46e16303e91f456780b9b39cc0
Zhitao Gong <zhitaao.gong@gmail.com>
2017-09-16 15:06:33 -0500

    add doc to jsma

--- attacks/jsma.py
+++ attacks/jsma.py
@@ -3,6 +3,29 @@ import tensorflow as tf
 
 def jsma(model, x, y, epochs=1.0, eps=1.0, clip_min=0.0, clip_max=1.0,
          pair=False, min_proba=0.0):
+    """
+    Jacobian-based saliency map approach.
+
+    See https://arxiv.org/abs/1511.07528 for details.
+
+    :param model: A wrapper that returns the output tensor of the model.
+    :param x: The input placeholder a 2D or 4D tensor.
+    :param y: The desired class label for each input, either an integer or a
+              list of integers.
+    :param epochs: Maximum epochs to run.  When it is a floating number in [0,
+        1], it is treated as the distortion factor, i.e., gamma in the original
+        paper.
+    :param eps: The noise added to input per epoch.
+    :param clip_min: The minimum value in output tensor.
+    :param clip_max: The maximum value in output tensor.
+    :param pair: If True, perturb two pixels at a time, otherwise one by one.
+    :param min_proba: The minimum probability the model produces the desired
+        target label given the adversarial samples.  The larger, the stronger
+        the adversarial samples.  If this is set to >1.0, then add noise until
+        the maximum epoch is reached.
+
+    :return: A tensor, contains adversarial samples for each input.
+    """
     xshape = tf.shape(x)
     n = xshape[0]
     target = tf.cond(tf.equal(0, tf.rank(y)),
@@ -19,9 +42,9 @@ def jsma(model, x, y, epochs=1.0, eps=1.0, clip_min=0.0, clip_max=1.0,
         _jsma_fn = _jsma_impl
 
     def _fn(i):
-        # `xi` is of the shape (1, ....), the first dimension is the
-        # number of samples, 1 in this case.  `yi` is just a scalar,
-        # denoting the target class index.
+        # `xi` is of the shape (1, ....), the first dimension is the number of
+        # samples, 1 in this case.  `yi` is just a scalar, denoting the target
+        # class index.
         xi = tf.gather(x, [i])
         yi = tf.gather(target, i)
 
@@ -31,12 +54,12 @@ def jsma(model, x, y, epochs=1.0, eps=1.0, clip_min=0.0, clip_max=1.0,
                         min_proba=min_proba)
         return xadv[0]
 
-    return tf.map_fn(_fn, tf.range(n), dtype=tf.float32,
-                     back_prop=False, name='jsma_batch')
+    return tf.map_fn(_fn, tf.range(n), dtype=tf.float32, back_prop=False,
+                     name='jsma_batch')
 
 
-def _jsma_impl(model, xi, yi, epochs, eps=1.0, clip_min=0.0,
-               clip_max=1.0, min_proba=0.0):
+def _jsma_impl(model, xi, yi, epochs, eps=1.0, clip_min=0.0, clip_max=1.0,
+               min_proba=0.0):
 
     def _cond(x_adv, epoch, pixel_mask):
         ybar = tf.reshape(model(x_adv), [-1])
@@ -92,15 +115,14 @@ def _jsma_impl(model, xi, yi, epochs, eps=1.0, clip_min=0.0,
                          lambda: tf.less(xi, clip_max),
                          lambda: tf.greater(xi, clip_min))
 
-    x_adv, _, _ = tf.while_loop(_cond, _body,
-                                (x_adv, epoch, pixel_mask),
+    x_adv, _, _ = tf.while_loop(_cond, _body, (x_adv, epoch, pixel_mask),
                                 back_prop=False, name='jsma_step')
 
     return x_adv
 
 
-def _jsma2_impl(model, xi, yi, epochs, eps=1.0, clip_min=0.0,
-                clip_max=1.0, min_proba=0.0):
+def _jsma2_impl(model, xi, yi, epochs, eps=1.0, clip_min=0.0, clip_max=1.0,
+                min_proba=0.0):
 
     def _cond(x_adv, epoch, pixel_mask):
         ybar = tf.reshape(model(x_adv), [-1])
@@ -135,10 +157,10 @@ def _jsma2_impl(model, xi, yi, epochs, eps=1.0, clip_min=0.0,
             count = tf.reduce_min([batch_size, n-start])
             ind3 = tf.slice(ind2, [start], [count])
 
-            # Selection C(n, 2), e.g., if n=4, a=[0 0 1 0 1 2], b=[1 2
-            # 2 3 3 3], the corresponding element in each array makes
-            # a pair, i.e., the pair index are store separately.  A
-            # special case is when there is only one pixel left.
+            # Selection C(n, 2), e.g., if n=4, a=[0 0 1 0 1 2], b=[1 2 2 3 3 3],
+            # the corresponding element in each array makes a pair, i.e., the
+            # pair index are store separately.  A special case is when there is
+            # only one pixel left.
             a, b = tf.meshgrid(ind3, ind3)
             c = tf.cond(tf.greater(count, 1),
                         lambda: tf.less(a, b),
@@ -189,13 +211,12 @@ def _jsma2_impl(model, xi, yi, epochs, eps=1.0, clip_min=0.0,
         v = tf.Variable(-1.)
         start = tf.Variable(0)
 
-        # Find max saliency pair in batch.  Naive iteration through
-        # the pair takes O(n^2).  Vectorized implementation may
-        # speedup the running time significantly, at the expense of
-        # O(n^2) space.  So Instead we find the max pair with batch
-        # max, during each batch we use vectorized implementation.
-        i, j, _, _ = tf.while_loop(_maxpair_batch_cond,
-                                   _maxpair_batch_body,
+        # Find max saliency pair in batch.  Naive iteration through the pair
+        # takes O(n^2).  Vectorized implementation may speedup the running time
+        # significantly, at the expense of O(n^2) space.  So Instead we find the
+        # max pair with batch max, during each batch we use vectorized
+        # implementation.
+        i, j, _, _ = tf.while_loop(_maxpair_batch_cond, _maxpair_batch_body,
                                    (i, j, v, start), back_prop=False)
 
         dx = tf.scatter_nd([i], [eps], tf.shape(x_adv)) +\
