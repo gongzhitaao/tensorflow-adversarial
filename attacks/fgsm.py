fb2a51798353f3a5924f6912dab4bf97155a1a5b
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-10 10:59:32 -0500

    fix bug fgsm for binary classification

--- attacks/fgsm.py
+++ attacks/fgsm.py
@@ -21,7 +21,7 @@ def fgsm(model, x, eps=0.01, epochs=1, clip_min=0., clip_max=1.):
     x_adv = tf.identity(x)
 
     ybar = model(x_adv)
-    yshape = tf.shape(ybar)
+    yshape = ybar.get_shape().as_list()
     ydim = yshape[1]
 
     indices = tf.argmax(ybar, axis=1)
@@ -30,6 +30,11 @@ def fgsm(model, x, eps=0.01, epochs=1, clip_min=0., clip_max=1.):
         lambda: tf.nn.relu(tf.sign(ybar - 0.5)),
         lambda: tf.one_hot(indices, ydim, on_value=1.0, off_value=0.0))
 
+    if 1 == ydim:
+        loss_fn = tf.nn.sigmoid_cross_entropy_with_logits
+    else:
+        loss_fn = tf.nn.softmax_cross_entropy_with_logits
+
     eps = tf.abs(eps)
 
     def _cond(x_adv, i):
@@ -37,8 +42,7 @@ def fgsm(model, x, eps=0.01, epochs=1, clip_min=0., clip_max=1.):
 
     def _body(x_adv, i):
         ybar, logits = model(x_adv, logits=True)
-        loss = tf.nn.softmax_cross_entropy_with_logits(
-            labels=target, logits=logits)
+        loss = loss_fn(labels=target, logits=logits)
         dy_dx, = tf.gradients(loss, x_adv)
         x_adv = tf.stop_gradient(x_adv + eps*tf.sign(dy_dx))
         x_adv = tf.clip_by_value(x_adv, clip_min, clip_max)
