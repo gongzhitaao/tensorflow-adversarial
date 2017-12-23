66c8d76858c1783ec0c10c52169a39de9fbf9cbf
Zhitao Gong <zhitaao.gong@gmail.com>
2017-12-23 13:27:57 -0600

    add deepfool batch mode example

---
+++ example/deepfool_mnist2_batch.py
@@ -0,0 +1,299 @@
+"""
+Use DeepFool to craft adversarial on binary labels.
+
+Binary labels are randomly selected from MNIST.
+
+Note that DeepFool assumes that the output of binary classifier is +1/-1,
+e.g., tf.tanh instead of tf.sigmoid.  As a result, we need MSE instead of
+cross entropy as loss function.
+"""
+import os
+
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')           # noqa: E402
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+import tensorflow as tf
+
+from attacks import deepfool
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+img_size = 28
+img_chan = 1
+
+
+print('\nLoading MNIST')
+
+mnist = tf.keras.datasets.mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = np.reshape(X_train, [-1, img_size, img_size, img_chan])
+X_train = X_train.astype(np.float32) / 255
+X_test = np.reshape(X_test, [-1, img_size, img_size, img_chan])
+X_test = X_test.astype(np.float32) / 255
+
+print('\nRandomly selecting two classes')
+# c0, c1 = np.random.choice(10, size=2, replace=False)
+c0, c1 = 8, 1
+
+i0 = y_train == c0
+i1 = y_train == c1
+X_train = np.vstack((X_train[i0], X_train[i1]))
+y_train = np.vstack((-np.ones([np.sum(i0), 1]), np.ones([np.sum(i1), 1])))
+
+i0 = y_test == c0
+i1 = y_test == c1
+X_test = np.vstack((X_test[i0], X_test[i1]))
+y_test = np.vstack((-np.ones([np.sum(i0), 1]), np.ones([np.sum(i1), 1])))
+# y_test = np.vstack((np.ones([np.sum(i0), 1]), np.zeros([np.sum(i1), 1])))
+
+print('\nSpliting data')
+
+ind = np.random.permutation(X_train.shape[0])
+X_train, y_train = X_train[ind], y_train[ind]
+
+VALIDATION_SPLIT = 0.1
+n = int(X_train.shape[0] * (1-VALIDATION_SPLIT))
+X_valid = X_train[n:]
+X_train = X_train[:n]
+y_valid = y_train[n:]
+y_train = y_train[:n]
+
+print('\nConstruction graph')
+
+
+def model(x, logits=False, training=False):
+    with tf.variable_scope('conv0'):
+        z = tf.layers.conv2d(x, filters=32, kernel_size=[3, 3],
+                             padding='same', activation=tf.nn.relu)
+        z = tf.layers.max_pooling2d(z, pool_size=[2, 2], strides=2)
+
+    with tf.variable_scope('conv1'):
+        z = tf.layers.conv2d(z, filters=64, kernel_size=[3, 3],
+                             padding='same', activation=tf.nn.relu)
+        z = tf.layers.max_pooling2d(z, pool_size=[2, 2], strides=2)
+
+    with tf.variable_scope('flatten'):
+        shape = z.get_shape().as_list()
+        z = tf.reshape(z, [-1, np.prod(shape[1:])])
+
+    with tf.variable_scope('mlp'):
+        z = tf.layers.dense(z, units=128, activation=tf.nn.relu)
+        z = tf.layers.dropout(z, rate=0.25, training=training)
+
+    logits_ = tf.layers.dense(z, units=1, name='logits')
+
+    # NOTE: DeepFool assumes outputs of +1/-1 for binary classifier.
+    y = tf.tanh(logits_, name='ybar')
+
+    if logits:
+        return y, logits_
+    return y
+
+
+class Dummy:
+    pass
+
+
+env = Dummy()
+
+
+with tf.variable_scope('model'):
+    env.x = tf.placeholder(tf.float32, (None, img_size, img_size, img_chan),
+                           name='x')
+    env.y = tf.placeholder(tf.float32, (None, 1), name='y')
+    env.training = tf.placeholder_with_default(False, (), name='mode')
+
+    env.ybar = model(env.x, training=env.training)
+
+    with tf.variable_scope('acc'):
+        count = tf.logical_not(tf.logical_xor(tf.greater(env.y, 0.0),
+                                              tf.greater(env.ybar, 0.0)))
+        env.acc = tf.reduce_mean(tf.cast(count, tf.float32), name='acc')
+
+    env.loss = tf.losses.mean_squared_error(labels=env.y,
+                                            predictions=env.ybar,
+                                            scope='loss')
+
+    with tf.variable_scope('train_op'):
+        optimizer = tf.train.AdamOptimizer()
+        env.train_op = optimizer.minimize(env.loss)
+
+    env.saver = tf.train.Saver()
+
+with tf.variable_scope('model', reuse=True):
+    env.adv_epochs = tf.placeholder(tf.int32, (), name='adv_epochs')
+    env.xadv = deepfool(model, env.x, epochs=env.adv_epochs, batch=True)
+
+print('\nInitializing graph')
+
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+sess.run(tf.local_variables_initializer())
+
+
+def evaluate(sess, env, X_data, y_data, batch_size=128):
+    """
+    Evaluate TF model by running env.loss and env.acc.
+    """
+    print('\nEvaluating')
+
+    n_sample = X_data.shape[0]
+    n_batch = int((n_sample+batch_size-1) / batch_size)
+    loss, acc = 0, 0
+
+    for batch in range(n_batch):
+        print(' batch {0}/{1}'.format(batch + 1, n_batch), end='\r')
+        start = batch * batch_size
+        end = min(n_sample, start + batch_size)
+        cnt = end - start
+        batch_loss, batch_acc = sess.run(
+            [env.loss, env.acc],
+            feed_dict={env.x: X_data[start:end],
+                       env.y: y_data[start:end]})
+        loss += batch_loss * cnt
+        acc += batch_acc * cnt
+    loss /= n_sample
+    acc /= n_sample
+
+    print(' loss: {0:.4f} acc: {1:.4f}'.format(loss, acc))
+    return loss, acc
+
+
+def train(sess, env, X_data, y_data, X_valid=None, y_valid=None, epochs=1,
+          load=False, shuffle=True, batch_size=128, name='model'):
+    """
+    Train a TF model by running env.train_op.
+    """
+    if load:
+        if not hasattr(env, 'saver'):
+            return print('\nError: cannot find saver op')
+        print('\nLoading saved model')
+        return env.saver.restore(sess, 'model/{}'.format(name))
+
+    print('\nTrain model')
+    n_sample = X_data.shape[0]
+    n_batch = int((n_sample+batch_size-1) / batch_size)
+    for epoch in range(epochs):
+        print('\nEpoch {0}/{1}'.format(epoch + 1, epochs))
+
+        if shuffle:
+            print('\nShuffling data')
+            ind = np.arange(n_sample)
+            np.random.shuffle(ind)
+            X_data = X_data[ind]
+            y_data = y_data[ind]
+
+        for batch in range(n_batch):
+            print(' batch {0}/{1}'.format(batch + 1, n_batch), end='\r')
+            start = batch * batch_size
+            end = min(n_sample, start + batch_size)
+            sess.run(env.train_op, feed_dict={env.x: X_data[start:end],
+                                              env.y: y_data[start:end],
+                                              env.training: True})
+        if X_valid is not None:
+            evaluate(sess, env, X_valid, y_valid)
+
+    if hasattr(env, 'saver'):
+        print('\n Saving model')
+        os.makedirs('model', exist_ok=True)
+        env.saver.save(sess, 'model/{}'.format(name))
+
+
+def predict(sess, env, X_data, batch_size=128):
+    """
+    Do inference by running env.ybar.
+    """
+    print('\nPredicting')
+    n_classes = env.ybar.get_shape().as_list()[1]
+
+    n_sample = X_data.shape[0]
+    n_batch = int((n_sample+batch_size-1) / batch_size)
+    yval = np.empty((n_sample, n_classes))
+
+    for batch in range(n_batch):
+        print(' batch {0}/{1}'.format(batch + 1, n_batch), end='\r')
+        start = batch * batch_size
+        end = min(n_sample, start + batch_size)
+        y_batch = sess.run(env.ybar, feed_dict={env.x: X_data[start:end]})
+        yval[start:end] = y_batch
+    print()
+    return yval
+
+
+def make_deepfool(sess, env, X_data, epochs=1, batch_size=128):
+    """
+    Generate FGSM by running env.xadv.
+    """
+    print('\nMaking adversarials via FGSM')
+
+    n_sample = X_data.shape[0]
+    n_batch = int((n_sample + batch_size - 1) / batch_size)
+    X_adv = np.empty_like(X_data)
+
+    for batch in range(n_batch):
+        print(' batch {0}/{1}'.format(batch + 1, n_batch), end='\r')
+        start = batch * batch_size
+        end = min(n_sample, start + batch_size)
+        adv = sess.run(env.xadv, feed_dict={env.x: X_data[start:end],
+                                            env.adv_epochs: epochs})
+        X_adv[start:end] = adv
+    print()
+
+    return X_adv
+
+
+print('\nTraining')
+
+train(sess, env, X_train, y_train, X_valid, y_valid, load=False, epochs=5,
+      name='mnist2')
+
+print('\nEvaluating on clean data')
+
+evaluate(sess, env, X_test, y_test)
+
+print('\nGenerating adversarial data')
+
+X_adv = make_deepfool(sess, env, X_test, epochs=2)
+
+print('\nEvaluating on adversarial data')
+
+evaluate(sess, env, X_adv, y_test)
+
+print('\nRandomly sample adversarial data from each category')
+
+y1 = predict(sess, env, X_test).flatten()
+y2 = predict(sess, env, X_adv).flatten()
+
+z0 = (y_test > 0).flatten().astype(np.int32)
+z1 = (y1 > 0).astype(np.int32)
+z2 = (y2 > 0).astype(np.int32)
+
+print('\nPlotting results')
+fig = plt.figure(figsize=(2, 2.2))
+gs = gridspec.GridSpec(2, 2)
+
+for i in range(2):
+    print('Target {0}'.format(i))
+    ind, = np.where(np.all([z0 == i, z1 == i, z2 != i], axis=0))
+    ind = np.random.choice(ind)
+    xcur = [X_test[ind], X_adv[ind]]
+    ycur = [y1[ind], y2[ind]]
+
+    for j in range(2):
+        img = np.squeeze(xcur[j])
+        ax = fig.add_subplot(gs[j, i])
+        ax.imshow(img, cmap='gray', interpolation='none')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xlabel('{0:.2f}'.format(ycur[j]), fontsize=10)
+
+print('\nSaving figure')
+gs.tight_layout(fig)
+os.makedirs('img', exist_ok=True)
+plt.savefig('img/deepfool_mnist2_batch.png')
